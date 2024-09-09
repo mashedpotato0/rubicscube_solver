297b017a4455c40f60ccd78b2e10e3828c0fdda7
Faziuddin Mohammad <106994894+mashedpotato0@users.noreply.github.com>
2024-09-10 01:12:12 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -89,7 +89,7 @@ class RubiksCube {
                             cell.style.backgroundColor = 'grey';
                             break;
                     }
-                    cell.textContent = value;
+                    //cell.textContent = value;
                     faceDiv.appendChild(cell);
                 });
             });
